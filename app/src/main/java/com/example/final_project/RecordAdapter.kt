1898e4b2c249c42d878adc65d6e3334e6ec0f088
Wu0308 <t111360141@ntut.org.tw>
2024-12-28 00:14:09 +0800

    add garbage icon

--- app/src/main/java/com/example/final_project/RecordAdapter.kt
+++ app/src/main/java/com/example/final_project/RecordAdapter.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
 class RecordAdapter(context: Context, private val records: List<String>) : ArrayAdapter<String>(context, 0, records) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_record, parent, false)
+
+
         val textView = view.findViewById<TextView>(R.id.tvRecord)
+        val imageView = view.findViewById<ImageView>(R.id.imageView2)
+
         textView.text = records[position]
+        imageView.setImageResource(R.drawable.ic_garbage)
+
         return view
     }
 }
